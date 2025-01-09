8f6c52747ffc35b09b64d550da9e03068f4cf0fe
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2025-01-09 13:22:09 +0100

    feat(yatzy.py): Refactorizar chance().

--- src/yatzy.py
+++ src/yatzy.py
@@ -1,14 +1,12 @@
 class Yatzy:
 
     @staticmethod
-    def chance(d1, d2, d3, d4, d5):
-        total = 0
-        total += d1
-        total += d2
-        total += d3
-        total += d4
-        total += d5
-        return total
+    def chance(*dices):
+        return sum(dices)
+    '''
+    En vez de sumarlos uno a uno los sumamos a la vez
+    con la funcion sum()
+    '''
 
     @staticmethod
     def yatzy(dice):
