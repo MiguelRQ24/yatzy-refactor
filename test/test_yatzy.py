7ee9d1fad1a95f99c287a79730fc0945ec9a09eb
Alberto <karcernucle4rthrone@gmail.com>
2025-01-09 13:35:42 +0100

    refactor(test_yatzy.py): refactorizar test_ones()

--- test/test_yatzy.py
+++ test/test_yatzy.py
@@ -15,8 +15,8 @@ def test_yatzy():
     assert 0 == Yatzy.yatzy(6, 6, 6, 6, 3)
 
 
-def test_1s():
-    assert Yatzy.ones(1, 2, 3, 4, 5) == 1
+def test_ones():
+    assert 1 == Yatzy.ones(1, 2, 3, 4, 5) 
     assert 2 == Yatzy.ones(1, 2, 1, 4, 5)
     assert 0 == Yatzy.ones(6, 2, 2, 4, 5)
     assert 4 == Yatzy.ones(1, 2, 1, 1, 1)
